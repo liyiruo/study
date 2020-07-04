f6664515acf0de3ab86851fd55cd34f6867741a4
liyiruo <401213245@qq.com>
2020-07-04 17:38:18 +0800

    设计模式之装饰模式

--- src/main/java/com/设计模式/_装饰模式2/Main.java
+++ src/main/java/com/设计模式/_装饰模式2/Main.java
@@ -5,10 +5,14 @@ package com.设计模式._装饰模式2;
  */
 public class Main {
     public static void main(String[] args) {
-       Component component = new ConcreteComponent();
-        Decorator decorator = new ConcreteDecoratorA(component);
-        decorator = new ConcreteDecoratorB(decorator);
+        Component component = new ConcreteComponent();
+        Decorator decorator = new ConcreteDecoratorB(component);
+        decorator = new ConcreteDecoratorA(decorator);
         decorator.operation();
+
+        new ConcreteDecoratorB(new ConcreteDecoratorA(new ConcreteComponent())).operation();
+        System.out.println("===================");
+        new ConcreteDecoratorA(new ConcreteDecoratorA(new ConcreteComponent())).operation();
 
 
     }
