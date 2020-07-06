89ef81ad20235bba63ecffa641ef51b04001e44d
李依若 <401213245@qq.com>
2020-07-06 20:48:27 +0800

    测试SubString

---
+++ src/main/java/com/测试/TestSubString.java
@@ -0,0 +1,12 @@
+package com.测试;
+
+/**
+ * @author Administrator
+ */
+public class TestSubString {
+
+    public static void main(String[] args) {
+        String s = "abcdef".substring(0, 2);
+        System.out.println(s);
+    }
+}
