152745c7ffea271ce3570fd63d3da03187d7305b
liyiruo <401213245@qq.com>
2020-10-12 22:51:22 +0800

    public int getHoldCount()方法的使用

---
+++ src/main/java/com/线程/多线程核心技术/_4/lockMethodTest1/MyService.java
@@ -0,0 +1,36 @@
+package com.线程.多线程核心技术._4.lockMethodTest1;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author liyiruo
+ * @Description
+ * @Date 2020/10/12 22:43
+ */
+public class MyService {
+    private ReentrantLock lock = new ReentrantLock(true);
+
+    public void testMethod1() {
+        System.out.println("A " + lock.getHoldCount());
+        lock.lock();
+        try {
+            System.out.println("B " + lock.getHoldCount());
+            testMethod2();
+            System.out.println("F " + lock.getHoldCount());
+        } finally {
+            lock.unlock();
+            System.out.println("G " + lock.getHoldCount());
+        }
+    }
+
+    public void testMethod2() {
+        System.out.println("C " + lock.getHoldCount());
+        lock.lock();
+        try {
+            System.out.println("D " + lock.getHoldCount());
+        } finally {
+            lock.unlock();
+            System.out.println("E " + lock.getHoldCount());
+        }
+    }
+}
---
+++ src/main/java/com/线程/多线程核心技术/_4/lockMethodTest1/Test.java
@@ -0,0 +1,14 @@
+package com.线程.多线程核心技术._4.lockMethodTest1;
+
+/**
+ * @author liyiruo
+ * @Description
+ * @Date 2020/10/12 22:47
+ */
+public class Test {
+    public static void main(String[] args) {
+        MyService service = new MyService();
+        service.testMethod1();
+       // service.testMethod2();
+    }
+}
