dc6e0eee68af4ebb64b73b3e6ca592308d626964
liyiruo <401213245@qq.com>
2020-10-13 15:48:09 +0800

    public final int getQueueLength()

---
+++ src/main/java/com/线程/多线程核心技术/_4/test2/Run.java
@@ -0,0 +1,28 @@
+package com.线程.多线程核心技术._4.test2;
+
+/**
+ * @author liyiruo
+ * @Description
+ * @Date 2020/10/13 15:36
+ */
+public class Run {
+    public static void main(String[] args) throws InterruptedException {
+        final Service service = new Service();
+        final int num=8;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                service.serviceMethod1();
+            }
+        };
+        Thread[] threadArray = new Thread[10];
+        for (int i = 0; i < num; i++) {
+            threadArray[i]=new Thread(runnable);
+        }
+        for (int i = 0; i < num; i++) {
+            threadArray[i].start();
+        }
+        Thread.sleep(2000);
+        System.out.println("有线程数：" + service.lock.getQueueLength() + "在等待获取锁");
+    }
+}
---
+++ src/main/java/com/线程/多线程核心技术/_4/test2/Service.java
@@ -0,0 +1,23 @@
+package com.线程.多线程核心技术._4.test2;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author liyiruo
+ * @Description
+ * @Date 2020/10/13 15:32
+ */
+public class Service {
+    public ReentrantLock lock = new ReentrantLock();
+    public void serviceMethod1() {
+        lock.lock();
+        try {
+            System.out.println("ThreadName=" + Thread.currentThread().getName() + "进入方法！");
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
