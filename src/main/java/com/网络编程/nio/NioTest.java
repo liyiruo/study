9282dea59c5dcb00bd545f70c0608eeb5b2a9905
liyiruo <401213245@qq.com>
2021-01-16 22:29:44 +0800

    nio

--- src/main/java/com/网络编程/nio/NioTest.java
+++ src/main/java/com/网络编程/nio/NioTest.java
@@ -7,6 +7,8 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * @author liyiruo
@@ -50,7 +52,7 @@ ch.socket().bind (new InetSocketAddress (somelocalport));
 
 
         SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.connect(new InetSocketAddress("loclhost",1000));
+        socketChannel.connect(new InetSocketAddress("loclhost", 1000));
 
         //SocketChannel ch = SocketChannel.close();
         //ch.connect(new InetSocketAddress("somehost", someport));
@@ -69,8 +71,54 @@ ch.socket().bind (new InetSocketAddress (somelocalport));
         char c = charBuffer.get();
 
         //分射读取操作
-        ScatteringByteChannel sc=null;
+        ScatteringByteChannel sc = null;
         //聚集写入操作
-        GatheringByteChannel gc=null;
+        GatheringByteChannel gc = null;
+
+        Selector selector = Selector.open();
+
+        ServerSocketChannel serverSocket = ServerSocketChannel.open();
+        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 8099);
+        serverSocket.bind(hostAddress);
+
+
+        /**
+         * 迭代所选的键集合来访问准备好的信道
+         */
+        Set<SelectionKey> selectedKeys = selector.selectedKeys();
+        Iterator<SelectionKey> selectionKeyIterator = selectedKeys.iterator();
+        while (selectionKeyIterator.hasNext()) {
+            SelectionKey key = selectionKeyIterator.next();
+
+            if (key.isConnectable()) {
+                // The connection was established with a remote server.
+            } else if (key.isAcceptable()) {
+                // The connection was accepted by a ServerSocketChannel.
+            } else if (key.isWritable()) {
+                //  The channel is ready for writing
+            } else if (key.isReadable()) {
+                // The channel is ready for reading
+            }
+
+            selectedKeys.remove(key);
+        }
+
+
+        Pipe pipe = Pipe.open();
+        Pipe.SourceChannel inChannel = pipe.source();
+        ByteBuffer byteBuffer1 = ByteBuffer.allocate(512);
+        int bytesRead = inChannel.read(byteBuffer1);
+
+
+        Pipe.SinkChannel SinkChannel = pipe.sink();
+        String newData = "The new String is writing in a Pipe..." + System.currentTimeMillis();
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(512 * 2);
+        byteBuffer2.clear();
+        byteBuffer2.put(newData.getBytes());
+        byteBuffer2.flip();
+        while (byteBuffer2.hasRemaining()){
+            SinkChannel.write(byteBuffer2);
+        }
+
     }
 }
